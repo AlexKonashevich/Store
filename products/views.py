eb8e1da11527a4d8f2df44699781aac264936a46
kon <karinae@inbox.ru>
2023-06-23 13:10:55 +0500

    Make associations site with database (categories and products)

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-
+from products.models import ProductCategory, Product
 # Create your views here.
 
 def index(request):
@@ -13,5 +13,7 @@ def index(request):
 def products(request):
     context = {
         'title': 'Каталог',
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
     }
     return render(request, 'products/products.html', context)

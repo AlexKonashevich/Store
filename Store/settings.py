6019c9f4d27cd935d977930e911cf22b4a7a95e7
kon <karinae@inbox.ru>
2023-07-26 09:52:07 +0500

    Add auth through GitHub

--- Store/settings.py
+++ Store/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.github',
 
     'products',
     'users',
@@ -156,3 +162,21 @@ EMAIL_PORT = 465
 EMAIL_HOST_USER = 'store-admi@yandex.ru'
 EMAIL_HOST_PASSWORD = 'hjxrkrbuufpnojat'
 EMAIL_USE_SSL = True
+
+# OAuth
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+# Provider specific settings
+SOCIALACCOUNT_PROVIDERS = {
+    'github': {
+        'SCOPE': [
+            'user',
+        ],
+    }
+}

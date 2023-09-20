5b963db01f78343bca1a963bc478d6dd27ad824c
kon <karinae@inbox.ru>
2023-09-20 08:50:29 +0500

    Add autocomplete to order form

--- orders/forms.py
+++ orders/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from django.db import models
+
 from orders.models import Order
-from users.models import User
 from django.views.generic.base import TemplateView
-from users.forms import UserProfileForm
+
+
 class OrdersForm(forms.ModelForm, TemplateView):
     first_name = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'form-control', 'placeholder': 'Иван'
@@ -17,13 +17,6 @@ class OrdersForm(forms.ModelForm, TemplateView):
     address = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'form-control', 'placeholder': 'Россия, Москва, ул. Мира, дом 6'
     }))
-    # model = User
-    # form_class = UserProfileForm
-    # template_name = 'users/profile.html'
-    # title = 'Store - Личный кабинет'
-    def get_context_data(self, **kwargs):
-        context = super(OrdersForm, self).get_context_data()
-        return context
 
     class Meta:
         model = Order
